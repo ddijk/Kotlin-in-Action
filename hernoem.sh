6f413df87993e1a785290758b8dae2db15f99a6a
ddijk <dick@dijkrosoft.nl>
2019-10-09 12:59:46 +0200

    Add comments

--- hernoem.sh
+++ hernoem.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 find . -name "*.kt" | while read file; do
-#   mv $file ${file//\./_}
+
+   # remove leading './'
     new="${file#"./"}"
+
+    # replace all '.' with '_'
     new2="${new//\./_}"
+
+    # restore last replacement back to '.'
     new3="${new2/_kt/.kt}"
+
     #echo "$file naar $new3" 
     mv $file $new3
 done
